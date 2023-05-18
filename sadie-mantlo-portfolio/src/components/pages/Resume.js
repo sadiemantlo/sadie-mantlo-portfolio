6ed73ce9b00e4d74e681b3fe829462bbd96ed319
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-05-17 20:59:01 -0700

    updated css

--- sadie-mantlo-portfolio/src/components/pages/Resume.js
+++ sadie-mantlo-portfolio/src/components/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from "react";
 import resume from "../../resume.jpg";
+import './pages.css';
 
 class Resume extends React.Component {
   downloadResume = () => {
@@ -9,31 +10,14 @@ class Resume extends React.Component {
     link.click();
   };
   render() {
-    const styles = {
-      card: {
-        width: "40rem",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: "110px"
-      },
-      img: {
-        width: "40rem",
-        display: "inline",
-      },
-      container: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      },
-    };
 
     return (
-        <div style={styles.container}>
-          <div className="card" style={styles.card}>
+        <div className='container'>
+          <div className="card">
             <img
               src="images/resume.jpg"
               alt="Sadie Mantlo Resume"
-              style={styles.img}
+              className='img'
             />
             <div className="card-body">
               <button onClick={this.downloadResume} style={{ textAlign: "center" }}>Download</button>
